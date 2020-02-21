5f6eb102cfaef9c55e173473a6b6b6f487eb47df
nasihs <nasihs1113@gmail.com>
2020-02-21 17:23:55 +0800

    1

--- test/udp_client.py
+++ test/udp_client.py
@@ -1,11 +1,12 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-for data in [b'Michael', b'Tracy', b'Sarah']:
-    # 发送数据:
-    s.sendto(data, ('192.168.50.36', 8888))
-    # 接收数据:
-    print(s.recv(1024).decode('utf-8'))
+while True:
+    for data in [b'Michael', b'Tracy', b'Sarah']:
+        # 发送数据:
+        # s.sendto(data, ('192.168.50.3', 7777))
+        s.sendto(data, ('223.104.211.188', 7777))
+        # 接收数据:
+        print(s.recv(1024).decode('utf-8'))
 
 s.close()
