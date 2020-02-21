55f117f4e7b9992808c8371777d7786abd5ca8d0
nasihs <nasihs1113@gmail.com>
2020-02-21 18:57:26 +0800

    updated

--- tcp_test/server1.py
+++ tcp_test/server1.py
@@ -7,7 +7,7 @@ import time
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 监听端口:
-s.bind(('127.0.0.1', 9999))
+s.bind(('223.104.211.245', 9999))
 
 s.listen(5)
 print('Waiting for connection...')
